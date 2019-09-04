3f8ce4fdda227565c864e1972368aa9a0e91fe19
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-09-04 14:52:14 +0200

    no padding

--- Tests/KeycardTests/CryptoTests.swift
+++ Tests/KeycardTests/CryptoTests.swift
@@ -10,8 +10,8 @@ final class CryptoTests: XCTestCase {
         let (sk, pk) = Crypto.shared.secp256k1GeneratePair()
         let secret = Crypto.shared.secp256k1ECDH(privKey: sk, pubKey: pk)
         let iv = Crypto.shared.random(count: SecureChannel.blockLength)
-        let encrypted = Crypto.shared.aes256Enc(data: plaintextBytes, iv: iv, key: secret)
-        let decrypted = Crypto.shared.aes256Dec(data: encrypted, iv: iv, key: secret)
+        let encrypted = Crypto.shared.aes256Enc(data: Crypto.shared.iso7816_4Pad(data: plaintextBytes, blockSize: SecureChannel.blockLength), iv: iv, key: secret)
+        let decrypted = Crypto.shared.iso7816_4Unpad(data: Crypto.shared.aes256Dec(data: encrypted, iv: iv, key: secret))
 
         let decryptedText = String(data: Data(decrypted), encoding: .utf8)
         XCTAssertEqual(decryptedText, plaintext)
