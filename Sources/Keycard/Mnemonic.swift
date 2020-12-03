367271eb2269f5050e051b02cd3625f3512ed58a
Bitgamma <opensource@bitgamma.com>
2020-12-03 11:39:15 +0300

    Update Mnemonic.swift
    make all methods public

--- Sources/Keycard/Mnemonic.swift
+++ Sources/Keycard/Mnemonic.swift
@@ -3,15 +3,15 @@ import Foundation
 public class Mnemonic {
     static let bip39IterationCount = 2048
     
-    static func toBinarySeed(mnemonicPhrase: String, password: String = "") -> [UInt8] {
+    public static func toBinarySeed(mnemonicPhrase: String, password: String = "") -> [UInt8] {
         Crypto.shared.pbkdf2(password: mnemonicPhrase,
                              salt: Array(("mnemonic" + password).utf8),
                              iterations: Mnemonic.bip39IterationCount,
                              hmac: PBKDF2HMac.sha512)
     }
     
-    let indexes: [UInt16]
-    var words: [String] {
+    public let indexes: [UInt16]
+    public var words: [String] {
         get {
             precondition(wordList != nil)
             return self.indexes.map { (idx) -> String in
@@ -20,9 +20,9 @@ public class Mnemonic {
         }
     }
     
-    var wordList: [String]?
+    public var wordList: [String]?
     
-    init(rawData: [UInt8]) {
+    public init(rawData: [UInt8]) {
         var idx: [UInt16] = []
         
         for i in 0..<(rawData.count / 2) {
@@ -32,19 +32,19 @@ public class Mnemonic {
         self.indexes = idx
     }
     
-    func useBIP39EnglishWordlist() {
+    public func useBIP39EnglishWordlist() {
         self.wordList = MnemonicEnglish.words
     }
     
-    func toMnemonicPhrase() -> String {
+    public func toMnemonicPhrase() -> String {
         self.words.joined(separator: " ")
     }
     
-    func toBinarySeed(password: String = "") -> [UInt8] {
+    public func toBinarySeed(password: String = "") -> [UInt8] {
         Mnemonic.toBinarySeed(mnemonicPhrase: toMnemonicPhrase(), password: password)
     }
     
-    func toBIP32KeyPair(password: String = "") -> BIP32KeyPair {
+    public func toBIP32KeyPair(password: String = "") -> BIP32KeyPair {
         BIP32KeyPair(fromSeed: toBinarySeed(password: password))
     }
 }
